1e3bf9badbeaec449437aaee3e6dce54806316ed
Adarsh Raj <adarshraj@Adarshs-Air.lan>
2024-11-14 11:45:20 +0530

    build setup

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -4,20 +4,28 @@ import { FullBlog } from "../components/FullBlog"
 import { BlogSkeleton } from "../components/BlogSkeleton"
 
 export const Blog = () => {
-    const {id} = useParams()
-    const {loading, blog} = useBlog({
-        id : id || ""
-    })
+    const { id } = useParams();
+    const { loading, blog } = useBlog({
+        id: id || ""
+    });
 
-    if(loading) {
-        return <div>
-            <BlogSkeleton />
-
-        </div>
+    if (loading) {
+        return (
+            <div>
+                <BlogSkeleton />
+            </div>
+        );
     }
+
+    // Check if blog is undefined, and return early if so
+    if (!blog) {
+        return <div>Blog not found.</div>;
+    }
+
     return (
         <div>
-            <FullBlog blog={blog}/>
+            {/* Now TypeScript knows that blog is not undefined */}
+            <FullBlog blog={blog} />
         </div>
-    )
-}
+    );
+};
